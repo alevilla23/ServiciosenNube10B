968ed338a901bda0647316e3ffdf2824c407d971
alevilla23 <sergiovr_2304@icloud.com>
2026-03-03 22:08:51 -0600

    login y notificaciones

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:copylogin/screens/login_screen.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:copylogin/screens/home_screen.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 
 void main() async{
@@ -10,17 +12,65 @@ void main() async{
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  String? token = await FirebaseMessaging.instance.getToken();
+  print("TOKEN: $token");
+
+  await FirebaseMessaging.instance.subscribeToTopic("prueba");
+
+
+  await _initMessaging();
   runApp(const MyApp());
+}
+
+Future<void> _initMessaging() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  // Pedir permisos
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  print('Permiso: ${settings.authorizationStatus}');
+
+  // Obtener token del dispositivo
+  String? token = await messaging.getToken();
+  print("TOKEN DEL DISPOSITIVO:");
+  print(token);
+
+  // Escuchar notificaciones cuando la app está abierta
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Mensaje recibido en primer plano: ${message.notification?.title}');
+  });
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
-    );
-  }
+Widget build(BuildContext context) {
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        print("USER ACTUAL: ${snapshot.data}");
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(child: CircularProgressIndicator()),
+          );
+        }
+
+        if (snapshot.hasData) {
+          return HomeScreen();
+        } else {
+          return LoginScreen();
+        }
+      },
+    ),
+  );
+}
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,126 +1,64 @@
 import 'package:flutter/material.dart';
-import '../services/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
-
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
+  _LoginScreenState createState() => _LoginScreenState();
 }
-
 
 class _LoginScreenState extends State<LoginScreen> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
-  final authService = AuthService();
-  
-  bool isLoading = false;
-  
-  //Metodo login que se encarga de validar los campos, mostrar un loading mientras se hace la petición y manejar la respuesta para navegar a HomeScreen o mostrar un error.
+
   Future<void> login() async {
-    final email = emailController.text.trim();
-    final password = passwordController.text.trim();
-
-     if (email.isEmpty || password.isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Completa todos los campos')),
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
       );
-      return;
-    }
-
-   setState(() => isLoading = true);
-
-   try {
-      final isValid = await authService.login(email, password);
-
-      if (!mounted) return;
-      // Si el login es exitoso, navegamos a HomeScreen. Si no, mostramos un mensaje de error.
-      if (isValid) {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (_) => const HomeScreen()),
-        );
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Usuario o contraseña inválidos')),
-        );
-      }
-    } catch (e) {
+    } on FirebaseAuthException catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Error de conexión')),
+        SnackBar(content: Text(e.message ?? "Error al iniciar sesión")),
       );
-    } finally {
-      if (mounted) {
-        setState(() => isLoading = false);
-      }
     }
   }
-  
 
-  //Aquí se construye la interfaz visual.
+  Future<void> register() async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+
+      print("Usuario registrado");
+    } catch (e) {
+      print("Error: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login')),
-       body: LayoutBuilder(  // Esto nos permite que sea responsive y se adapte a diferentes tamaños de pantalla.
-        builder: (context, constraints) {
-          return SingleChildScrollView(  // Esto permite que el contenido este centrado y no ocurra un overflow cuando el teclado aparezca.
-            child: Center(
-              child: ConstrainedBox(
-                constraints: BoxConstraints(
-                  maxWidth: constraints.maxWidth > 600 ? 400 : double.infinity,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Bienvenido',
-                        style: Theme.of(context).textTheme.headlineMedium,
-                      ),
-                      const SizedBox(height: 30),
-                      TextField(
-                        controller: emailController,
-                        decoration: const InputDecoration(
-                          labelText: 'Email',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                      TextField(
-                        controller: passwordController,
-                        decoration: const InputDecoration(
-                          labelText: 'Password',
-                          border: OutlineInputBorder(),
-                        ),
-                        obscureText: true,
-                      ),
-                      const SizedBox(height: 24),
-                      SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: isLoading ? null : login,
-                          child: isLoading
-                              ? const  SizedBox(
-                                  width: 20,
-                                  height: 20,
-                                  child: CircularProgressIndicator(
-                                    strokeWidth: 2,
-                                    color: Colors.white,
-                                  ),
-                                )
-                              : const Text('Ingresar'),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: InputDecoration(labelText: "Email"),
             ),
-          );
-        },
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(labelText: "Password"),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: login, child: Text("Iniciar sesión")),
+            ElevatedButton(onPressed: register, child: Text("Registrarse")),
+          ],
+        ),
       ),
     );
   }
